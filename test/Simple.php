623ec6f174c40c53a0eb9c1139befc06be056a27
varzer <randomfun@163.com>
2021-12-07 11:50:01 +0800

    'test'

---
+++ test/Simple.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * Author   :   SetMe
+ * Time     :   2021/12/7 11:48
+ * FileName :   Simple.php
+ * github   :   http://github.com/varzer
+ */
+
+class Simple extends \varzer\service\Base
+{
+
+    protected function createExcelTHead(array $data)
+    {
+        // TODO: Implement createExcelTHead() method.
+    }
+
+    protected function createExcelTBody(array $data)
+    {
+        // TODO: Implement createExcelTBody() method.
+    }
+}
